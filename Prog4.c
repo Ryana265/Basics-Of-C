76e8ba7ba89e5ed322c7aa1b9eeb2636f4d42165
Ryana265 <99040464+Ryana265@users.noreply.github.com>
2023-08-10 17:08:36 +0530

    Create Prog4.c

---
+++ Prog4.c
@@ -0,0 +1,14 @@
+// Write a program to check whether a given character is Alphabet or not using if else statement 
+#include <stdio.h>
+int main() {
+    char c;
+    printf("Enter a character: ");
+    scanf("%c", &c);
+
+    if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+        printf("%c is an alphabet.", c);
+    else
+        printf("%c is not an alphabet.", c);
+
+    return 0;
+}
