09c6244d05c1d1b91b6b2f52b35e4d422c2dc909
Ryana265 <99040464+Ryana265@users.noreply.github.com>
2023-08-10 11:53:20 +0530

    Create Prog1.c

---
+++ Prog1.c
@@ -0,0 +1,11 @@
+//Course id and Credit from user//
+#include <stdio.h>
+int main(){
+    int cid;
+    float cr;
+    printf("Enter your course id and credit : ");
+    scanf("%d%f",&cid,&cr);
+    printf("Your Course Id is :%d",cid);
+    printf("Your Credit is :%f",cr);
+    return 0;
+}
