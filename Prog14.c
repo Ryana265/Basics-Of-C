84c2de05c59d8573c1596342bd3df43d5ffef5ec
Ryana265 <99040464+Ryana265@users.noreply.github.com>
2023-08-13 20:50:10 +0530

    Create Prog14.c

---
+++ Prog14.c
@@ -0,0 +1,16 @@
+//simple C program that checks whether a given number is even or odd
+#include <stdio.h>
+int main() {
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (num % 2 == 0) {
+        printf("%d is an even number.\n", num);
+    } else {
+        printf("%d is an odd number.\n", num);
+    }
+
+    return 0;
+}
